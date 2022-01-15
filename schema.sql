3b9a5393af908b5ae021b9693e28e724cfa0cec3
Luke Oguro <lukeoguro@gmail.com>
2022-01-14 21:40:06 -0500

    [+] Add files

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE expenses (
+  id serial PRIMARY KEY,
+  amount decimal(6,2)
+    NOT NULL
+    CHECK (amount > 0),
+  memo text NOT NULL,
+  created_on date NOT NULL
+);
+
+INSERT INTO expenses (amount, memo, created_on)
+VALUES (14.56, 'Pencils', NOW()),
+(3.29, 'Coffee', NOW()),
+(49.99, 'Text Editor', NOW());
